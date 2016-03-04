8af8bc8bf9d4727ade19a6b7179dcfb75c6b21ba
Elliot Wright <wright.elliot@gmail.com>
2016-03-04 23:26:15 +0000

    #1: Tests for InputValidator.

--- src/test/scala/eidolon/console/input/validation/InputValidatorSpec.scala
+++ src/test/scala/eidolon/console/input/validation/InputValidatorSpec.scala
@@ -11,72 +11,246 @@
 
 package eidolon.console.input.validation
 
-import org.scalatest.{BeforeAndAfter, FunSpec}
+import eidolon.console.input.definition.InputDefinition
+import eidolon.console.input.definition.parameter.{InputOption, InputArgument}
+import eidolon.console.input.parser.parameter.{ParsedInputShortOption, ParsedInputLongOption, ParsedInputArgument}
+import eidolon.console.input.validation.parameter.{InvalidOption, ValidOption, ValidArgument, InvalidArgument}
+import org.scalatest.FunSpec
 
 /**
  * InputValidator Spec
  *
  * @author Elliot Wright <elliot@elliotwright.co>
  */
-class InputValidatorSpec extends FunSpec with BeforeAndAfter {
+class InputValidatorSpec extends FunSpec {
   describe("eidolon.console.input.validation.InputValidator") {
     describe("validate()") {
       describe("when validating arguments") {
         it("should set arguments that exist in the result as valid arguments") {
-          pending
+          val validator = new InputValidator()
+
+          val definition = new InputDefinition(arguments = Map(
+            "arg1" -> new InputArgument("arg1"),
+            "arg2" -> new InputArgument("arg2")
+          ))
+
+          val result = validator.validate(definition, List(
+            new ParsedInputArgument("foo"),
+            new ParsedInputArgument("bar"),
+            new ParsedInputArgument("baz")
+          ))
+
+          assert(result.validArguments.exists({
+            case ValidArgument(name, argVal) => name == "arg1" && argVal == "foo"
+          }))
+
+          assert(result.validArguments.exists({
+            case ValidArgument(name, argVal) => name == "arg2" && argVal == "bar"
+          }))
         }
 
-        it("should set arguments that don't exist in the result as invalid arguments") {
-          pending
+        it("should not set arguments that don't exist in the result") {
+          val validator = new InputValidator()
+
+          val definition = new InputDefinition(arguments = Map(
+            "arg1" -> new InputArgument("arg1"),
+            "arg2" -> new InputArgument("arg2")
+          ))
+
+          val result = validator.validate(definition, List())
+
+          assert(result.invalidArguments.isEmpty)
+          assert(result.validArguments.isEmpty)
         }
 
         it("should set missing required arguments in the result as invalid arguments") {
-          pending
+          val validator = new InputValidator()
+
+          val definition = new InputDefinition(arguments = Map(
+            "arg1" -> new InputArgument("arg1", mode = InputArgument.REQUIRED)
+          ))
+
+          val result = validator.validate(definition, List())
+
+          assert(result.invalidArguments.size == 1)
+          assert(result.invalidArguments.exists({
+            case InvalidArgument(token) => token == "arg1"
+          }))
         }
 
         it("should set missing arguments with default values in the result as valid arguments") {
-          pending
+          val validator = new InputValidator()
+
+          val definition = new InputDefinition(arguments = Map(
+            "arg1" -> new InputArgument("arg1", mode = InputArgument.OPTIONAL, default = Some("val1"))
+          ))
+
+          val result = validator.validate(definition, List())
+
+          assert(result.validArguments.size == 1)
+          assert(result.validArguments.exists({
+            case ValidArgument(token, argVal) => token == "arg1" && argVal == "val1"
+          }))
         }
       }
 
       describe("when validating long options") {
         describe("that expect no value") {
           it("should set them in the result as valid options") {
-            // case InputOption.VALUE_NONE
-            pending
+            val validator = new InputValidator()
+
+            val definition = new InputDefinition(options = Map(
+              "opt1" -> new InputOption("opt1", mode = InputOption.VALUE_NONE)
+            ))
+
+            val result = validator.validate(definition, List(
+              new ParsedInputLongOption("opt1", None)
+            ))
+
+            assert(result.validOptions.size == 1)
+            assert(result.validOptions.exists({
+              case ValidOption(name, optVal) => name == "opt1" && optVal.isEmpty
+            }))
           }
         }
 
         describe("that have an optional value") {
           it("should set them as valid options in the result with the given value if one is provided") {
-            // case InputOption.VALUE_OPTIONAL, value not empty
-            pending
+            val validator = new InputValidator()
+
+            val definition = new InputDefinition(options = Map(
+              "opt1" -> new InputOption(
+                name = "opt1",
+                mode = InputOption.VALUE_OPTIONAL
+              )
+            ))
+
+            val result = validator.validate(definition, List(
+              new ParsedInputLongOption("opt1", Some("val1"))
+            ))
+
+            assert(result.validOptions.size == 1)
+            assert(result.validOptions.exists({
+              case ValidOption(name, optVal) => name == "opt1" && optVal.contains("val1")
+            }))
           }
 
           it("should set them as valid options in the result with the default value if one is set and no value is given") {
-            // case InputOption.VALUE_OPTIONAL, value empty, default not empty
-            pending
+            val validator = new InputValidator()
+
+            val definition = new InputDefinition(options = Map(
+              "opt1" -> new InputOption(
+                name = "opt1",
+                mode = InputOption.VALUE_OPTIONAL,
+                defaultValue = Some("val1")
+              )
+            ))
+
+            val result = validator.validate(definition, List(
+              new ParsedInputLongOption("opt1", None)
+            ))
+
+            assert(result.validOptions.size == 1)
+            assert(result.validOptions.exists({
+              case ValidOption(name, optVal) => name == "opt1" && optVal.contains("val1")
+            }))
           }
 
           it("should set them as valid options in the result with no value if there is no value provided, and no default value") {
-            // case InputOption.VALUE_OPTIONAL, value empty, default empty
-            pending
+            val validator = new InputValidator()
+
+            val definition = new InputDefinition(options = Map(
+              "opt1" -> new InputOption(
+                name = "opt1",
+                mode = InputOption.VALUE_OPTIONAL
+              )
+            ))
+
+            val result = validator.validate(definition, List(
+              new ParsedInputLongOption("opt1", None)
+            ))
+
+            assert(result.validOptions.size == 1)
+            assert(result.validOptions.exists({
+              case ValidOption(name, optVal) => name == "opt1" && optVal.isEmpty
+            }))
           }
         }
 
         describe("that require a value") {
           it("should set them as valid options in the result with the given value if one is provided") {
-            pending
+            val validator = new InputValidator()
+
+            val definition = new InputDefinition(options = Map(
+              "opt1" -> new InputOption(
+                name = "opt1",
+                mode = InputOption.VALUE_REQUIRED
+              )
+            ))
+
+            val result = validator.validate(definition, List(
+              new ParsedInputLongOption("opt1", Some("val1"))
+            ))
+
+            assert(result.validOptions.size == 1)
+            assert(result.validOptions.exists({
+              case ValidOption(name, optVal) => name == "opt1" && optVal.contains("val1")
+            }))
           }
 
           it("should set them as invalid options in the result if no value is provided") {
-            pending
+            val validator = new InputValidator()
+
+            val definition = new InputDefinition(options = Map(
+              "opt1" -> new InputOption(
+                name = "opt1",
+                mode = InputOption.VALUE_REQUIRED
+              )
+            ))
+
+            val result = validator.validate(definition, List(
+              new ParsedInputLongOption("opt1", None)
+            ))
+
+            assert(result.invalidOptions.size == 1)
+            assert(result.invalidOptions.exists({
+              case InvalidOption(token) => token == "opt1"
+            }))
+          }
+        }
+
+        describe("that are not defined but were given") {
+          it("should set them as invalid options in the result") {
+            val validator = new InputValidator()
+            val definition = new InputDefinition(options = Map())
+
+            val result = validator.validate(definition, List(
+              new ParsedInputLongOption("opt1", None)
+            ))
+
+            assert(result.invalidOptions.size == 1)
+            assert(result.invalidOptions.exists({
+              case InvalidOption(token) => token == "opt1"
+            }))
           }
         }
 
         describe("that have an unknown mode") {
           it("should throw throw an exception") {
-            pending
+            val validator = new InputValidator()
+
+            val definition = new InputDefinition(options = Map(
+              "opt1" -> new InputOption(
+                name = "opt1",
+                mode = 3 // numbers not divisible by 2 aren't possible
+              )
+            ))
+
+            intercept[RuntimeException] {
+              validator.validate(definition, List(
+                new ParsedInputLongOption("opt1", None)
+              ))
+            }
           }
         }
       }
@@ -84,41 +258,166 @@ class InputValidatorSpec extends FunSpec with BeforeAndAfter {
       describe("when validating short options") {
         describe("that expect no value") {
           it("should set them in the result as valid options") {
-            // case InputOption.VALUE_NONE
-            pending
+            val validator = new InputValidator()
+
+            val definition = new InputDefinition(options = Map(
+              "opt1" -> new InputOption("opt1", Some("o"), mode = InputOption.VALUE_NONE)
+            ))
+
+            val result = validator.validate(definition, List(
+              new ParsedInputShortOption("o", None)
+            ))
+
+            assert(result.validOptions.size == 1)
+            assert(result.validOptions.exists({
+              case ValidOption(name, optVal) => name == "opt1" && optVal.isEmpty
+            }))
           }
         }
 
         describe("that have an optional value") {
           it("should set them as valid options in the result with the given value if one is provided") {
-            // case InputOption.VALUE_OPTIONAL, value not empty
-            pending
+            val validator = new InputValidator()
+
+            val definition = new InputDefinition(options = Map(
+              "opt1" -> new InputOption(
+                name = "opt1",
+                shortName = Some("o"),
+                mode = InputOption.VALUE_OPTIONAL
+              )
+            ))
+
+            val result = validator.validate(definition, List(
+              new ParsedInputShortOption("o", Some("val1"))
+            ))
+
+            assert(result.validOptions.size == 1)
+            assert(result.validOptions.exists({
+              case ValidOption(name, optVal) => name == "opt1" && optVal.contains("val1")
+            }))
           }
 
           it("should set them as valid options in the result with the default value if one is set and no value is given") {
-            // case InputOption.VALUE_OPTIONAL, value empty, default not empty
-            pending
+            val validator = new InputValidator()
+
+            val definition = new InputDefinition(options = Map(
+              "opt1" -> new InputOption(
+                name = "opt1",
+                shortName = Some("o"),
+                mode = InputOption.VALUE_OPTIONAL,
+                defaultValue = Some("val1")
+              )
+            ))
+
+            val result = validator.validate(definition, List(
+              new ParsedInputShortOption("o", None)
+            ))
+
+            assert(result.validOptions.size == 1)
+            assert(result.validOptions.exists({
+              case ValidOption(name, optVal) => name == "opt1" && optVal.contains("val1")
+            }))
           }
 
           it("should set them as valid options in the result with no value if there is no value provided, and no default value") {
-            // case InputOption.VALUE_OPTIONAL, value empty, default empty
-            pending
+            val validator = new InputValidator()
+
+            val definition = new InputDefinition(options = Map(
+              "opt1" -> new InputOption(
+                name = "opt1",
+                shortName = Some("o"),
+                mode = InputOption.VALUE_OPTIONAL
+              )
+            ))
+
+            val result = validator.validate(definition, List(
+              new ParsedInputShortOption("o", None)
+            ))
+
+            assert(result.validOptions.size == 1)
+            assert(result.validOptions.exists({
+              case ValidOption(name, optVal) => name == "opt1" && optVal.isEmpty
+            }))
           }
         }
 
         describe("that require a value") {
           it("should set them as valid options in the result with the given value if one is provided") {
-            pending
+            val validator = new InputValidator()
+
+            val definition = new InputDefinition(options = Map(
+              "opt1" -> new InputOption(
+                name = "opt1",
+                shortName = Some("o"),
+                mode = InputOption.VALUE_REQUIRED
+              )
+            ))
+
+            val result = validator.validate(definition, List(
+              new ParsedInputShortOption("o", Some("val1"))
+            ))
+
+            assert(result.validOptions.size == 1)
+            assert(result.validOptions.exists({
+              case ValidOption(name, optVal) => name == "opt1" && optVal.contains("val1")
+            }))
           }
 
           it("should set them as invalid options in the result if no value is provided") {
-            pending
+            val validator = new InputValidator()
+
+            val definition = new InputDefinition(options = Map(
+              "opt1" -> new InputOption(
+                name = "opt1",
+                shortName = Some("o"),
+                mode = InputOption.VALUE_REQUIRED
+              )
+            ))
+
+            val result = validator.validate(definition, List(
+              new ParsedInputShortOption("o", None)
+            ))
+
+            assert(result.invalidOptions.size == 1)
+            assert(result.invalidOptions.exists({
+              case InvalidOption(token) => token == "opt1"
+            }))
+          }
+        }
+
+        describe("that are not defined but were given") {
+          it("should set them as invalid options in the result") {
+            val validator = new InputValidator()
+            val definition = new InputDefinition(options = Map())
+
+            val result = validator.validate(definition, List(
+              new ParsedInputShortOption("o", None)
+            ))
+
+            assert(result.invalidOptions.size == 1)
+            assert(result.invalidOptions.exists({
+              case InvalidOption(token) => token == "o"
+            }))
           }
         }
 
         describe("that have an unknown mode") {
           it("should throw throw an exception") {
-            pending
+            val validator = new InputValidator()
+
+            val definition = new InputDefinition(options = Map(
+              "opt1" -> new InputOption(
+                name = "opt1",
+                shortName = Some("o"),
+                mode = 3 // numbers not divisible by 2 aren't possible
+              )
+            ))
+
+            intercept[RuntimeException] {
+              validator.validate(definition, List(
+                new ParsedInputShortOption("o", None)
+              ))
+            }
           }
         }
       }
